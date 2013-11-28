ff778632867373252efca70d8748e20f1e99ce15
rjking58 <richardjking2000@yahoo.com>
2013-11-28 11:03:53 -0800

    reflection learning

---
+++ csharp/ReflectionLearn/ReflectionLearn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReflectionLearn
+{
+    class ClassToTest
+    {
+        public int method1(int i)
+        {
+            return i + i;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
