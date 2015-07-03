07240e113624176ddf0d4a9b2b0b21308b7ec398
rjking58 <richardjking2000@yahoo.com>
2015-07-03 01:57:06 -0700

    Simple Tracer with unit tests also demonstrating parallels tasks.

---
+++ csharp/SimpleTrace/SimpleTrace/RJKSimpleTrace.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrace
+{
+    public class RJKSimpleTrace
+    {
+        static bool s_initialized = false;
+        static StreamWriter s_sw = null;
+        static Object s_writeLock = new object();
+        static Object s_initlock = new object();
+        public RJKSimpleTrace(string filename)
+        {
+            lock (s_initlock)
+            {
+                if (!s_initialized)
+                {
+                    s_sw = new StreamWriter(filename, true);
+                    s_initialized = true;
+                }
+            }
+        }
+
+        public RJKSimpleTrace()
+        {
+            if(!s_initialized)
+            {
+                throw new InvalidOperationException("must be constructed first time with a filename");
+            }
+        }
+
+        public void closeTrace()
+        {
+            lock(s_writeLock)
+            {
+                if(s_sw != null)
+                {
+                    s_sw.Close();
+                    s_sw = null;
+                    s_initialized = false;
+                }
+            }
+        }
+        public void WriteLine(string outputLine)
+        {
+            lock(s_writeLock)
+            {
+                if(s_sw != null)
+                {
+                    StackTrace st = new StackTrace(true);
+                    StackFrame sf = st.GetFrame(1);
+
+                    s_sw.WriteLine(string.Format(   "{0}[{1}] : {2}",
+                                                    sf.GetFileName(),
+                                                    sf.GetFileLineNumber(),
+                                                    outputLine));
+                    s_sw.Flush();
+                }
+            }
+        }
+    }
+}
---
+++ csharp/SimpleTrace/TestTracer/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿#define DEBUG
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTrace;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestTracer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+        }
+
+        private int m_TaskID = 0;
+        public int GetNextID()
+        {
+            return ++m_TaskID;
+        }
+
+        [TestMethod]
+        public void SimpleTrace_BasicFunctionality()
+        {
+            RJKSimpleTrace st = new RJKSimpleTrace("c:\\temp\\trace1.txt");
+            st.WriteLine("a");
+            st.WriteLine("b");
+            st.WriteLine("c");
+            st.WriteLine("d");
+            st.WriteLine("e");
+
+            st.closeTrace();
+        }
+
+
+
+        [TestMethod]
+        public void SimpleTrace_ParallelTaskTest()
+        {
+
+            RJKSimpleTrace st = new RJKSimpleTrace("c:\\temp\\parallel.txt");
+
+            Action printVals = () =>
+                {
+                    int id = GetNextID();
+                    RJKSimpleTrace stx = new RJKSimpleTrace();
+                    for (int val = 0; val < 1000; val++)
+                        stx.WriteLine(string.Format("[{0}]:{1}", id, val));
+                };
+            Task tA = new Task(printVals);
+            Task tB = new Task(printVals);
+            Task tC = new Task(printVals);
+
+            tA.Start();
+            tB.Start();
+            tC.Start();
+
+            tA.Wait();
+            tB.Wait();
+            tC.Wait();
+            st.closeTrace();
+
+        }
+    }
+}
